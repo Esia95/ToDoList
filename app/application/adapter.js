0d20456d4b230b5815fb32d250af4a4aca8f40a4
Estera Nowak <esteranowak95@gmail.com>
2022-08-27 12:36:45 +0200

    adding route, application, show task

---
+++ app/application/adapter.js
@@ -0,0 +1,9 @@
+import RESTAdapter from '@ember-data/adapter/rest';
+
+export default class ApplicationAdapter extends RESTAdapter {
+  host = 'https://db2c-185-78-75-249.ngrok.io';
+
+  get headers() {
+    return { 'owner-secret': 'bMDo1dFMDY' };
+  }
+}
